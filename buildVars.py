6fdc0b83695941b0652fa0465a3feb24526c7c18
Bryn <briexists@gmail.com>
2023-04-03 18:36:57 -0500

    Update for compatibility with NVDA 2023.1 and bump version. Also change add-on URL.

--- buildVars.py
+++ buildVars.py
@@ -19,15 +19,15 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Adds 3D audio for controls and replaces control messages. This updated version uses Synthizer and is compatible with NVDA 2022.1."""),
 	# version
-	"addon_version" : "0.9.4",
+	"addon_version" : "0.9.5",
 	# Author(s)
 	"addon_author" : "Camlorn <camlorn38@gmail.com>, Bryan Smart< Bryansmart@bryansmart.com>, Masonasons <mason@masonasons.me>",
 	# URL for the add-on documentation support
-	"addon_url" : "https://github.com/masonasons/unspoken",
+	"addon_url" : "https://github.com/brynify/unspoken",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	"addon_minimum_nvda_version" : "2021.1",
-	"addon_last_tested_nvda_version" : "2022.1",
+	"addon_last_tested_nvda_version" : "2023.1",
 }
 
 
