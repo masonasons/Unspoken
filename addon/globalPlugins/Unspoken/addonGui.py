732a09158e6cb627e15bfeb51eff2b2732a74df4
Bryn <briexists@gmail.com>
2023-04-03 20:29:22 -0500

    Change labels of check boxes in the add-on prefs panel to make the settings more clear. Also essentially reverse the check boxes so that existing settings remain compatible. Fixes #5

--- addon/globalPlugins/Unspoken/addonGui.py
+++ addon/globalPlugins/Unspoken/addonGui.py
@@ -8,13 +8,13 @@ class SettingsPanel(gui.SettingsPanel):
 	title = "Unspoken"
 	def makeSettings(self, settingsSizer):
 		settingsSizer = guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
-		self.sayAllCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="&Disable in say all"))
-		self.sayAllCheckBox.SetValue(config.conf["unspoken"]["sayAll"])
+		self.sayAllCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="&Play sounds during say all"))
+		self.sayAllCheckBox.SetValue((True if config.conf["unspoken"]["sayAll"]==False else False))
 		self.speakRolesCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="&Speak object roles"))
 		self.speakRolesCheckBox.SetValue(config.conf["unspoken"]["speakRoles"])
-		self.HRTFCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="Enable &HRTF"))
+		self.HRTFCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="Use &HRTF (3D Sound)"))
 		self.HRTFCheckBox.SetValue(config.conf["unspoken"]["HRTF"])
-		self.ReverbCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="Enable &Reverb (Requires NVDA Restart)"))
+		self.ReverbCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="Use &Reverb (Requires NVDA Restart)"))
 		self.ReverbCheckBox.SetValue(config.conf["unspoken"]["Reverb"])
 		self.ReverbLevelSliderLabel = settingsSizer.addItem(wx.StaticText(self, label="Reverb Level"))
 		self.ReverbLevelSlider = settingsSizer.addItem(wx.Slider(self))
@@ -22,8 +22,8 @@ class SettingsPanel(gui.SettingsPanel):
 		self.ReverbTimeSliderLabel = settingsSizer.addItem(wx.StaticText(self, label="Reverb Time"))
 		self.ReverbTimeSlider = settingsSizer.addItem(wx.Slider(self))
 		self.ReverbTimeSlider.SetValue(config.conf["unspoken"]["ReverbTime"]*100)
-		self.noSoundsCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="Don't &play sounds for roles"))
-		self.noSoundsCheckBox.SetValue(config.conf["unspoken"]["noSounds"])
+		self.noSoundsCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="&play sounds for roles (Enable Add-On)"))
+		self.noSoundsCheckBox.SetValue((True if config.conf["unspoken"]["noSounds"]==False else False))
 		self.volumeCheckBox = settingsSizer.addItem(wx.CheckBox(self, label="Automatically adjust sounds with speech &volume"))
 		self.volumeCheckBox.SetValue(config.conf["unspoken"]["volumeAdjust"])
 
@@ -31,10 +31,10 @@ class SettingsPanel(gui.SettingsPanel):
 		self.sayAllCheckBox.SetFocus()
 
 	def onSave(self):
-		if self.noSoundsCheckBox.IsChecked() and not self.speakRolesCheckBox.IsChecked():
+		if not self.noSoundsCheckBox.IsChecked() and not self.speakRolesCheckBox.IsChecked():
 			gui.messageBox("Disabling both sounds and  speaking is not allowed. NVDA will not say roles like button and checkbox, and sounds won't play either. Please change one of these settings", "Error")
 			return
-		config.conf["unspoken"]["sayAll"] = self.sayAllCheckBox.IsChecked()
+		config.conf["unspoken"]["sayAll"] = not self.sayAllCheckBox.IsChecked()
 		config.conf["unspoken"]["speakRoles"] = self.speakRolesCheckBox.IsChecked()
 		sound.context.default_panner_strategy.value=synthizer.PannerStrategy.STEREO if not config.conf['unspoken']['HRTF'] else synthizer.PannerStrategy.HRTF
 		config.conf["unspoken"]["HRTF"] = self.HRTFCheckBox.IsChecked()
@@ -43,5 +43,5 @@ class SettingsPanel(gui.SettingsPanel):
 		sound.reverb.gain.value=config.conf["unspoken"]["ReverbLevel"]
 		config.conf["unspoken"]["ReverbTime"] = self.ReverbTimeSlider.GetValue()/100
 		sound.reverb.t60.value=config.conf["unspoken"]["ReverbTime"]
-		config.conf["unspoken"]["noSounds"] = self.noSoundsCheckBox.IsChecked()
+		config.conf["unspoken"]["noSounds"] = not self.noSoundsCheckBox.IsChecked()
 		config.conf["unspoken"]["volumeAdjust"] = self.volumeCheckBox.IsChecked()
